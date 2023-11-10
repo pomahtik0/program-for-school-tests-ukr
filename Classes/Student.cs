ea85fe3e1b2f0cc916f9a6d535990d79fbd76fae
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-10 13:48:36 +0200

    some student fields explained

--- Classes/Student.cs
+++ Classes/Student.cs
@@ -8,5 +8,12 @@ namespace program_for_school_tests_ukr.Classes
 {
     internal class Student : User
     {
+        //поле з історією тестування
+        
+        //класс
+
+        //коментарі про учня, від інших вчителів
+
+        //можливо якась додаткова інформація
     }
 }
