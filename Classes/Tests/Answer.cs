2e73594adc0b5129dc19230a722e0ad256e37765
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-10 13:24:26 +0200

    Added class .cs files

---
+++ Classes/Tests/Answer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_for_school_tests_ukr.Classes.Tests
+{
+    internal class Answer
+    {
+    }
+}
