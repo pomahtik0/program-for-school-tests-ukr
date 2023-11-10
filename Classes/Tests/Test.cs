610b2728548c55b7d157e4a62d183b8560251b4d
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-10 14:00:00 +0200

    Some test fields created

--- Classes/Tests/Test.cs
+++ Classes/Tests/Test.cs
@@ -8,5 +8,17 @@ namespace program_for_school_tests_ukr.Classes.Tests
 {
     internal class Test
     {
+        public string Name { get; private set; }
+        public Teacher Owner { get; init; }
+        public bool IsRandomOrdered { get; private set; }
+        public bool GetRandomTests { get; private set; } // Обрати випадкові запитання зі списку
+        public int QuestionCount { get; private set; } // Кількість запитань
+        public int[] LeveledQuestionCount { get; init; } // Кількість запитань
+
+        //результат виражається оцінкою чи відповіддю?
+
+        //поле з історією проходження цього тесту
+
+
     }
 }
