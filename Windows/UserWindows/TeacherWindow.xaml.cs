b42d1a69e2cbfab38e6c2baea227856595127d18
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-20 14:28:01 +0200

    exit button behaviour

--- Windows/UserWindows/TeacherWindow.xaml.cs
+++ Windows/UserWindows/TeacherWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using program_for_school_tests_ukr.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,10 @@ namespace program_for_school_tests_ukr.Windows
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-
+            User.CurrentUser = null;
+            BaseWindow baseWindow = new BaseWindow();
+            baseWindow.Show();
+            this.Close();
         }
     }
 }
