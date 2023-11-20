607c95928db1763f6054b6e3c2aa075a4bce0a53
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-20 10:34:49 +0200

    Student .ToString method override

--- Classes/Student.cs
+++ Classes/Student.cs
@@ -9,11 +9,16 @@ namespace program_for_school_tests_ukr.Classes
     internal class Student : User
     {
         //поле з історією тестування
-        
+
         //класс
 
         //коментарі про учня, від інших вчителів
 
         //можливо якась додаткова інформація
+
+        public override string ToString()
+        {
+            return "Student";
+        }
     }
 }
