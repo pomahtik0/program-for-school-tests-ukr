be146a0679a19ef0101c8ebf5181affa677e74fa
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-23 11:13:27 +0200

    Aplication Context Class

--- Classes/User.cs
+++ Classes/User.cs
@@ -18,21 +18,4 @@ namespace program_for_school_tests_ukr.Classes
         public string Password { get; set; }
         public static User? CurrentUser {get; set;}
     }
-
-    internal class UserContext : DbContext
-    {
-        public DbSet<User> Users { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\pomahtik\source\repos\program for school tests ukr\Database\Database.mdf"";Integrated Security=True;Connect Timeout=30");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>()
-                .HasDiscriminator<string>("User_type")
-                .HasValue<Student>("Student")
-                .HasValue<Teacher>("Teacher");
-        }
-    }
 }
---
+++ Database/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using program_for_school_tests_ukr.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_for_school_tests_ukr.Database
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\pomahtik\source\repos\program for school tests ukr\Database\Database.mdf"";Integrated Security=True;Connect Timeout=30");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasDiscriminator<string>("User_type")
+                .HasValue<Student>("Student")
+                .HasValue<Teacher>("Teacher");
+        }
+    }
+}
