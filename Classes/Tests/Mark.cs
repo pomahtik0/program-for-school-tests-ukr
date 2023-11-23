d7fb95bab08accd6dd384e73b8d8594cf3d157b3
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-23 11:26:38 +0200

    Mark for test class

---
+++ Classes/Tests/Mark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_for_school_tests_ukr.Classes.Tests
+{
+    internal class Mark
+    {
+        public int CurrentMark { get; set; }
+        public int MaxMark { get; set; }
+        public Test Test { get; set; }
+        public Student Student { get; set; }
+    }
+}
