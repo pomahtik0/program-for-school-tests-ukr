6f6e8822f5aa2da7631042a141b41b707b4666ef
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-27 14:34:52 +0200

    authwindow unnesesary null defenition

--- Windows/AuthWindow.xaml.cs
+++ Windows/AuthWindow.xaml.cs
@@ -42,7 +42,6 @@ namespace program_for_school_tests_ukr.Windows
                 else
                 {
                     MessageBox.Show("неправильний логін, або пароль... потім перероблю");
-                    User.CurrentUser = null;
                 }
             }
         }
