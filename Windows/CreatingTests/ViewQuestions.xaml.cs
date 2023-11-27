cb75e033ff42625afd45ec24d4304fd1f64fd3cc
Roman Havriluk <pomahtik_inc@outlook.com>
2023-11-27 10:02:40 +0200

    setting curent test in init

--- Windows/CreatingTests/ViewQuestions.xaml.cs
+++ Windows/CreatingTests/ViewQuestions.xaml.cs
@@ -24,6 +24,7 @@ namespace program_for_school_tests_ukr.Windows.CreatingTests
         public Test currentTest;
         public ViewQuestions(int testId)
         {
+            SetCurrentTest(testId);
             InitializeComponent();
         }
 
