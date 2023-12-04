6a99cea660cf0c869307b9d30c8404733b2d1476
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-04 14:33:12 +0200

    non-needed properties removed

--- Classes/Tests/Test.cs
+++ Classes/Tests/Test.cs
@@ -26,8 +26,6 @@ namespace program_for_school_tests_ukr.Classes.Tests
         public Subject TestSubject { get; set; }
         public bool IsRandomOrdered { get; set; }
         public bool IsTrainingTest { get; set; }
-        public bool GetRandomTests { get; set; } // Обрати випадкові запитання зі списку
-        public int QuestionCount { get; set; } // Кількість запитань
         public List<Mark> Marks { get; set; } = new List<Mark>();
         public List<Question> questions { get; set; } = new List<Question>();
     }
