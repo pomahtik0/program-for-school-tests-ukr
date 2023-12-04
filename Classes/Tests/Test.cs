131bebb35f5299ebb26c93b2ea040d3b03f3c06d
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-04 14:32:21 +0200

    IsTraining test, and comments removed

--- Classes/Tests/Test.cs
+++ Classes/Tests/Test.cs
@@ -25,11 +25,10 @@ namespace program_for_school_tests_ukr.Classes.Tests
         public string Description { get; set; }
         public Subject TestSubject { get; set; }
         public bool IsRandomOrdered { get; set; }
+        public bool IsTrainingTest { get; set; }
         public bool GetRandomTests { get; set; } // Обрати випадкові запитання зі списку
         public int QuestionCount { get; set; } // Кількість запитань
-        //public int[] LeveledQuestionCount { get; set; } = new int[3];// Кількість
         public List<Mark> Marks { get; set; } = new List<Mark>();
         public List<Question> questions { get; set; } = new List<Question>();
-        //предмет
     }
 }
