4f4e506b8e8bffb1c82aeb19c85be1542be10cd3
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-04 17:30:24 +0200

    answer model

--- Classes/Tests/Answer.cs
+++ Classes/Tests/Answer.cs
@@ -8,5 +8,9 @@ namespace program_for_school_tests_ukr.Classes.Tests
 {
     public class Answer
     {
+        public int Id { get; set; }
+        public bool IsRealAnswer {  get; set; }
+        public string answerText {  get; set; }
+        public Question answerToQuestion { get; set; }
     }
 }
