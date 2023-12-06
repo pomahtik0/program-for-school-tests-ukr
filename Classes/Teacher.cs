337dae6f83c57f1ac1acda9718307d899bbdb609
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-06 10:35:25 +0200

    TODO removed

--- Classes/Teacher.cs
+++ Classes/Teacher.cs
@@ -10,8 +10,6 @@ namespace program_for_school_tests_ukr.Classes
     public class Teacher : User
     {
         public List<Test> OwnedTests { get; } = new List<Test>();
-
-        //поле для пройдених учнями тестів з оцінками, потім реалізую
         public override string ToString()
         {
             return "Teacher";
