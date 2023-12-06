0fcb201e603b7858858c5ad15a1cc04401d23bda
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-06 09:36:16 +0200

    Adding sample data to db

--- Windows/LoadingWindow.xaml.cs
+++ Windows/LoadingWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using program_for_school_tests_ukr.Classes;
+using program_for_school_tests_ukr.Classes.Tests;
+using program_for_school_tests_ukr.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,18 @@ namespace program_for_school_tests_ukr.Windows
         public LoadingWindow()
         {
             InitializeComponent();
+            using (var dbcontext = new ApplicationContext())
+            {
+                var stud1 = new Student() { Name = "st", Username = "stud1", Password = "stud1" };
+                var stud2 = new Student() { Name = "stu", Username = "stud2", Password = "stud2" };
+                var teacher1 = new Teacher() { Name = "teacher", Username = "teacher1", Password = "teacher1" };
+                Test test = new Test() { Name = "test1", Owner = teacher1, Description = "test", IsRandomOrdered = true, DateOfLastRedaction = DateOnly.FromDateTime(DateTime.Now), TestSubject = Subject.Математика, IsTrainingTest = false };
+                dbcontext.Users.Add(stud1);
+                dbcontext.Users.Add(stud2);
+                dbcontext.Users.Add(teacher1);
+                dbcontext.Tests.Add(test);
+                dbcontext.SaveChanges();
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
