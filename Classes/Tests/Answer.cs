3aee0c643c61209c76927a44e8b3b2cc274a8cb9
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-12 13:13:54 +0200

    answer types added

--- Classes/Tests/Answer.cs
+++ Classes/Tests/Answer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Controls;
 
 namespace program_for_school_tests_ukr.Classes.Tests
@@ -12,5 +14,34 @@ namespace program_for_school_tests_ukr.Classes.Tests
         public int Id { get; set; }
         public abstract UserControl Show();
         public abstract UserControl ShowInRedactMode();
+    }
+
+    public class TextAnswer : Answer
+    {
+        public string Text { get; set; }
+
+        public override UserControl Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override UserControl ShowInRedactMode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PictureAnswer : Answer
+    {
+        public byte[] Picture { get; set; }
+        public override UserControl Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override UserControl ShowInRedactMode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
