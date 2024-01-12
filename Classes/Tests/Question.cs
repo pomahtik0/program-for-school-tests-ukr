57fb6c6ec2381557d5d2295031d6f0e230e64fc4
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-12 21:07:02 +0200

    Question as text

--- Classes/Tests/Question.cs
+++ Classes/Tests/Question.cs
@@ -10,13 +10,23 @@ namespace program_for_school_tests_ukr.Classes.Tests
     public abstract class Question
     {
         public int Id { get; private set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public List<Answer> Answers { get; } = new List<Answer>();
-        protected Question(string name)
-        {
-            Name = name;
-        }
         public abstract Page Show();
         public abstract Page ShowInRedactMode();
+    }
+
+    public class QuestionAsText : Question
+    {
+        public string Text { get; set; } = "";
+        public override Page Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Page ShowInRedactMode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
