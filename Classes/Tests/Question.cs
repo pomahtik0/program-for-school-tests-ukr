5c3989d4da074d9a460d6a6d1418e82e99a27684
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-12 20:59:57 +0200

    question as abstract class

--- Classes/Tests/Question.cs
+++ Classes/Tests/Question.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace program_for_school_tests_ukr.Classes.Tests
 {
-    public class Question
+    public abstract class Question
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Name { get; set; }
-        public string QuestionText { get; set; }
         public List<Answer> Answers { get; } = new List<Answer>();
-        public string AnswerDescription { get; set; }
+        protected Question(string name)
+        {
+            Name = name;
+        }
+        public abstract Page Show();
+        public abstract Page ShowInRedactMode();
     }
 }
