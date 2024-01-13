2f6f446ae3c9130611fa2f400f6a23a6a85bf8ac
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-13 12:03:09 +0200

    Test class is now abstract

--- Classes/Tests/Test.cs
+++ Classes/Tests/Test.cs
@@ -19,24 +19,25 @@ namespace program_for_school_tests_ukr.Classes.Tests
         Історія,
         Інше
     }
-    public class Test
+
+    public abstract class Test (Teacher owner)
     {
         public int Id { get; private set; }
-        public string Name { get; set; }
-        public Teacher Owner { get; private set; }
+        public string Name { get; set; } = string.Empty;
+        public Teacher Owner { get; private set; } = owner;
         public DateOnly DateOfLastRedaction { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
         public Subject TestSubject { get; set; }
-        public bool IsRandomOrdered { get; set; }
         public bool IsTrainingTest { get; set; }
-        public int MaxMark { get; set; }
+        public ushort MaxMark { get; set; }
+        public int TimeForTest { get; set; }
         public List<Mark> Marks { get; } = new List<Mark>();
         public List<Question> Questions { get; } = new List<Question>();
-        public Test(Teacher teacher)
-        {
-            Owner = teacher;
-            Name = "";
-            Description = "";
-        }
+    }
+
+    public class SimpleTest(Teacher owner) : Test(owner) 
+    {
+        public bool IsRandomOrdered { get; set; }
+        
     }
 }
