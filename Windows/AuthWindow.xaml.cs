cd4e90d1d8a4b737e7bf73da22d74de1cd1aa3dd
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-13 14:18:16 +0200

    message box message changed

--- Windows/AuthWindow.xaml.cs
+++ Windows/AuthWindow.xaml.cs
@@ -41,7 +41,7 @@ namespace program_for_school_tests_ukr.Windows
                 }
                 else
                 {
-                    MessageBox.Show("неправильний логін, або пароль... потім перероблю");
+                    MessageBox.Show("неправильний логін, або пароль...");
                 }
             }
         }
