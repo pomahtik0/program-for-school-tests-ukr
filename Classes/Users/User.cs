83e6dd7f25549d2414e3cb4af6a777faad22ce19
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-13 13:39:36 +0200

    user class updated

--- Classes/Users/User.cs
+++ Classes/Users/User.cs
@@ -6,15 +6,30 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.Metadata;
+using System.Windows;
+using System.Windows.Navigation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace program_for_school_tests_ukr.Classes.Users
 {
     public abstract class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public static User CurrentUser { get; set; }
+        [Unicode(false)]
+        [MaxLength(20)]
+        public string Username { get; set; } = string.Empty;
+        [Unicode]
+        [MaxLength(120)]
+        public string Name { get; set; } = string.Empty;
+        [Unicode(false)]
+        [MaxLength(20)]
+        public string Password { get; set; } = string.Empty;
+        [NotMapped]
+        protected Window? currentWindow;
+        public virtual Window GetWindow()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
