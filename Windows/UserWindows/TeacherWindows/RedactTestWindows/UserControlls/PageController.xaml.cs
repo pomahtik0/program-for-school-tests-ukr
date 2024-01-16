02500fec78eff2643ae6ce04e346d50a9b07befd
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-16 11:35:10 +0200

    new userconroll atributes

--- Windows/UserWindows/TeacherWindows/RedactTestWindows/UserControlls/PageController.xaml.cs
+++ Windows/UserWindows/TeacherWindows/RedactTestWindows/UserControlls/PageController.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using program_for_school_tests_ukr.Classes.Tests;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +23,26 @@ namespace program_for_school_tests_ukr.Windows.UserWindows.TeacherWindows.Redact
     /// </summary>
     public partial class PageController : UserControl
     {
+        private static readonly DependencyPropertyKey questionsPropertyKey =
+        DependencyProperty.RegisterReadOnly(
+          name: "ListOfQuestions",
+          propertyType: typeof(IEnumerable<Question>),
+          ownerType: typeof(PageController),
+          typeMetadata: new FrameworkPropertyMetadata()
+        );
+        public IEnumerable<Question> ListOfQuestions =>
+            (IEnumerable<Question>)GetValue(questionsPropertyKey.DependencyProperty);
+
+        public static readonly DependencyProperty PageWindowProperty = DependencyProperty.Register(
+            "PageWindow", typeof(Control),
+            typeof(PageController)
+            );
+
+        public Control PageWindow
+        {
+            get => (Control)GetValue(PageWindowProperty);
+            set => SetValue(PageWindowProperty, value);
+        }
         public PageController()
         {
             InitializeComponent();
