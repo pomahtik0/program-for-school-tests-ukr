b5e91771b76f37f7a5ea290b89a4393bd202ceed
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-17 13:37:18 +0200

    back conversion applied

--- Classes/Converters/IsCheckedToActualAnswerConverter.cs
+++ Classes/Converters/IsCheckedToActualAnswerConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 
 namespace program_for_school_tests_ukr.Classes.Converters
 {
-    [ValueConversion(typeof(Answer), typeof(bool), ParameterType = typeof(Question))]
+    [ValueConversion(typeof(Answer), typeof(bool), ParameterType = typeof(Answer))]
     internal class IsCheckedToActualAnswerConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -19,7 +19,12 @@ namespace program_for_school_tests_ukr.Classes.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            bool isChecked = (bool)value;
+            if(isChecked)
+            {
+                return parameter;
+            }
+            return value;
         }
     }
 }
