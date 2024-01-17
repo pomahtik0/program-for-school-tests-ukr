e6aeac8269fa7441229b324406b980c21b702c4d
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-17 13:34:06 +0200

    new converter created

---
+++ Classes/Converters/IsCheckedToActualAnswerConverter.cs
@@ -0,0 +1,25 @@
+﻿using program_for_school_tests_ukr.Classes.Tests;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace program_for_school_tests_ukr.Classes.Converters
+{
+    [ValueConversion(typeof(Answer), typeof(bool), ParameterType = typeof(Question))]
+    internal class IsCheckedToActualAnswerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
