fcaa589684d869cd8bad77ce70d1f9f63741b670
Roman Havriluk <pomahtik_inc@outlook.com>
2024-01-17 12:29:51 +0200

    setting answer to textAnswerControll

--- Windows/UserWindows/TeacherWindows/RedactTestWindows/UserControlls/AnswerAsTextControl.xaml.cs
+++ Windows/UserWindows/TeacherWindows/RedactTestWindows/UserControlls/AnswerAsTextControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using program_for_school_tests_ukr.Classes.Tests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,11 @@ namespace program_for_school_tests_ukr.Windows.UserWindows.TeacherWindows.Redact
     /// </summary>
     public partial class AnswerAsTextControl : UserControl
     {
-        public AnswerAsTextControl()
+        TextAnswer textAnswer;
+        public AnswerAsTextControl(TextAnswer textAnswer)
         {
             InitializeComponent();
+            this.textAnswer = textAnswer;
         }
     }
 }
